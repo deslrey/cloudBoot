4f2ed89bd840bec5260f7ce55d82e9a5ce44ef6a
deslre <2135232170@qq.com>
2024-07-28 21:11:07 +0800

    实现文件信息表

---
+++ src/main/java/org/deslre/utils/ScaleFilter.java
@@ -0,0 +1,63 @@
+package org.deslre.utils;
+
+
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+/**
+ * ClassName: ScaleFilter
+ * Description: TODO
+ * Author: Deslrey
+ * Date: 2024-07-28 21:05
+ * Version: 1.0
+ */
+public class ScaleFilter {
+
+    private static final Logger log = LoggerFactory.getLogger(ScaleFilter.class);
+
+
+    public static void createCover4Video(File sourceFile, Integer width, File targetFile) {
+        try {
+            String cmd = "ffmpeg -i %s -y -vframes 1 -vf scale=%d:%d/a %s";
+            ProcessUtils.executeCommand(String.format(cmd, sourceFile.getAbsoluteFile(), width, width, targetFile.getAbsoluteFile()), false);
+        } catch (Exception e) {
+            log.error("生成视频封面失败", e);
+        }
+    }
+
+    public static Boolean createThumbnailWidthFFmpeg(File file, int thumbnailWidth, File targetFile, Boolean delSource) {
+        try {
+            BufferedImage src = ImageIO.read(file);
+            //thumbnailWidth 缩略图的宽度   thumbnailHeight 缩略图的高度
+            int sourceW = src.getWidth();
+            int sourceH = src.getHeight();
+            //小于 指定高宽不压缩
+            if (sourceW <= thumbnailWidth) {
+                return false;
+            }
+            compressImage(file, thumbnailWidth, targetFile, delSource);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static void compressImage(File sourceFile, Integer width, File targetFile, Boolean delSource) {
+        try {
+            String cmd = "ffmpeg -i %s -vf scale=%d:-1 %s -y";
+            ProcessUtils.executeCommand(String.format(cmd, sourceFile.getAbsoluteFile(), width, targetFile.getAbsoluteFile()), false);
+            if (delSource) {
+                FileUtils.forceDelete(sourceFile);
+            }
+        } catch (Exception e) {
+            log.error("压缩图片失败");
+        }
+    }
+
+}
