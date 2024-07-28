29d6a480ea1bbf940262d75c9e768a7db404f042
deslre <2135232170@qq.com>
2024-07-28 17:13:25 +0800

    初始化后端项目

---
+++ src/main/java/org/deslre/CloudBootApplication.java
@@ -0,0 +1,14 @@
+package org.deslre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CloudBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudBootApplication.class, args);
+    }
+
+}
